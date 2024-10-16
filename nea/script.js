8599b2b42f0e02ea7306e7fadc4442f7aaf3fc81
aseega <156912117+aseega@users.noreply.github.com>
2024-10-16 20:04:02 +0100

    Update script.js

--- nea/script.js
+++ nea/script.js
@@ -1,6 +1,6 @@
 const hourHand = document.querySelector('.hour-hand');
 const minuteHand = document.querySelector('.minute-hand');
-const secondHand = document.querySelector('.second-hand');
+const digitalClock = document.getElementById('digitalClock');
 
 function setClock() {
     const now = new Date();
@@ -15,6 +15,12 @@ function setClock() {
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+    digitalClock.innerText = formatDigitalTime(hours, minutes);
+}
+
+function formatDigitalTime(hours, minutes) {
+    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
 }
 
 setInterval(setClock, 1000);
